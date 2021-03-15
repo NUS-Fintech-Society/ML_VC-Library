58d6db85ab907e9f2d4b4b2cb61eb3f0b3c9d78a
huangminggg <huangming.low@fundingsocieties.com>
2021-03-15 18:00:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import logging
 from mlvc.config import config
-from mlvc.logging.logger import Logger
+from argparse import ArgumentParser
+import os
+from datetime import datetime
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--mode",
+        dest="mode",
+        help="start mode, train, download_data" " backtest",
+        metavar="MODE",
+        default="download_data",
+    )
+    return parser
 
 
 def main():
-    Logger(config.LOGGING_LEVEL)
     logging.info("Logger successfully initialized, running main.py")
+    parser = build_parser()
+    options = parser.parse_args()
+    if not os.path.exists("./" + config.DATA_SAVE_DIR):
+        os.makedirs("./" + config.DATA_SAVE_DIR)
+    if options.mode == "download_data":
+        from mlvc.data.crunch_base import CrunchBaseScrapper
+        df = CrunchBaseScrapper(headless=False).fetch_data(start=config.START_RANKING, end=config.END_RANKING)
+        now = datetime.now().strftime("%Y%m%d")
+        df.to_csv(f"./{config.DATA_SAVE_DIR}/{now}_{config.START_RANKING}_{config.END_RANKING}.csv", index=False)
 
 
 if __name__ == "__main__":
