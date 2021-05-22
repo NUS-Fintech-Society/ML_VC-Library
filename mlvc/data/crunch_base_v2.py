753319fd49a82172e68263dcffff1a1eca284f44
huangmingg <huangmingg@hotmail.com>
2021-05-22 17:33:22 +0800

    Added option to backup data to csvfile

--- mlvc/data/crunch_base_v2.py
+++ mlvc/data/crunch_base_v2.py
@@ -8,11 +8,18 @@ import time
 
 class CrunchBaseScrapper_v2:
     driver = None
+    name_header = 'company_name'
+    url_header = 'company_url'
 
     def __init__(self, headless=False):
         self.options = selenium.webdriver.ChromeOptions()
         self.options.add_argument('--start-maximized')
         self.headless = headless
+
+    @staticmethod
+    def create_empty_header_file(filepath):
+        df = pd.DataFrame({ CrunchBaseScrapper_v2.name_header: [], CrunchBaseScrapper_v2.url_header: [] })
+        df.to_csv(filepath, index=False)
 
     def _go_company_ranking(self, ranking):
         if not self.driver:
@@ -26,14 +33,16 @@ class CrunchBaseScrapper_v2:
             ActionChains(self.driver).release(element).perform()
             time.sleep(3)
 
-    def fetch_data(self, start=100001, end=300000, step=15):
+    def fetch_data(self, start=100001, end=300000, step=15, backup_filepath=None):
         all_names = []
         all_links = []
         for i in range(start, end, step):
             if not self.driver:
                 self.driver = start_chrome(headless=self.headless, options=self.options)
+
             self._go_company_ranking(i)
             time.sleep(2)
+            
             if Text("Please verify you are a human").exists():
                 element = self.driver.find_element_by_id('px-captcha').find_element_by_tag_name("iframe")
                 ActionChains(self.driver).click_and_hold(element).perform()
@@ -41,20 +50,23 @@ class CrunchBaseScrapper_v2:
                 ActionChains(self.driver).release(element).perform()
                 time.sleep(3)
                 self._go_company_ranking(i)
+            
             name_list = [cell.web_element.text for cell in find_all(
                 S("div > grid-row > grid-cell > div > field-formatter > identifier-formatter > a",
                   below="Organization Name"))]
+            
             link_list = [cell.web_element.get_attribute("href") for cell in find_all(
                 S("div > grid-row > grid-cell > div > field-formatter > identifier-formatter > a",
                   below="Organization Name"))]
-            for name in name_list:
-                all_names.append(name)
-            for link in link_list:
-                all_links.append(link)
-            # kill_browser()
-        df = pd.DataFrame({'Company Name': all_names,
-                           'URL': all_links})
-        return df
+
+            if backup_filepath:
+                df = pd.DataFrame({ self.name_header: name_list, self.url_header: link_list })
+                df.to_csv(backup_filepath, mode='a', header=False, index=False)
+            
+            all_names.append(name_list)
+            all_links.append(link_list)
+
+        return pd.DataFrame({ self.name_header: all_names, self.url_header: all_links })
 
     def _check_driver(self):
         if not self.driver:
