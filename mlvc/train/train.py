d16bf7274f7f24ed0748ab0dce8591c9223a3245
eram576 <71161528+eram576@users.noreply.github.com>
2021-07-27 22:42:24 +0800

    updated based on latest xgb model

--- mlvc/train/train.py
+++ mlvc/train/train.py
@@ -15,16 +15,27 @@ class Train():
         self.name = name
 
     def read_data(self):
+        """Reads the data
+        Returns
+        -------
+        dataframe
+        """
         df = pd.read_csv('./Data/full_information_0_60k.csv')
         return df
 
     def train_model(self):
+        """Trains the model
+        Returns
+        -------
+        testing and training f1 scores
+        """
+
         df = Crunchbase().format_crunchbase()
-        subset = df[['type_Group B', 'money_raised_at_ipo', 'number_of_acquisitions', 'type_Unicorn',
-                     'employee_cat', 'valuation_at_ipo', 'number_of_investments', 'type_Group A',
-                     'type_Emerging Unicorn', 'number_of_lead_investments', 'number_of_lead_investors',
-                     'number_of_employee_profiles',
-                     'industries_type_2', 'type_For Profit', 'total_products_active', 'ipo_status']]
+        subset = df[['type_Group B', 'money_raised_at_ipo', 'number_of_acquisitions', 'valuation_at_ipo',
+                     'employee_cat', 'number_of_lead_investors', 'number_of_lead_investments', 'number_of_investments',
+                     'type_Group A', 'industries_type_0', 'number_of_employee_profiles',
+                     'number_of_events',
+                     'number_of_investors', 'total_products_active', 'type_For Profit', 'ipo_status']]
         x = subset[subset.columns.drop('ipo_status')]
         y = subset[['ipo_status']]
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
@@ -37,7 +48,7 @@ class Train():
         pickle.dump(scaler, open(f"./Model/scaler_{today}.pkl", 'wb'))
         x_test_scaled = scaler.transform(x_test)
 
-        # transform data to balance it
+        # transform imbalanced training data to balance it
         smote = SMOTE(random_state=42, sampling_strategy=0.5)
         x_train_scaled_smote, y_train_smote = smote.fit_resample(x_train_scaled, y_train)
 
